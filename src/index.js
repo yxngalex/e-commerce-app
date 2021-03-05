cb38fda41901f3ead1ba220889b3e46bbdae2c3b
Aleksa <aleksa.cekic8@gmail.com>
2021-03-05 14:02:11 +0100

    moved everything to its own file, and reducer

--- src/index.js
+++ src/index.js
@@ -2,19 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+
+import {store, persistor} from './redux/store/store';
 
 import './index.scss';
 import App from './App';
 
-import store from './redux/store/store';
-
 ReactDOM.render(
     <Provider store={store}>
-        <BrowserRouter>
-            <React.StrictMode>
-                <App/>
-            </React.StrictMode>
-        </BrowserRouter>
+        <PersistGate persistor={persistor}>
+            <BrowserRouter>
+                <React.StrictMode>
+                    <App/>
+                </React.StrictMode>
+            </BrowserRouter>
+        </PersistGate>
     </Provider>,
     document.getElementById('root')
 );
